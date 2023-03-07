0714ae54dab5eceea803e8a57f0ce1983de240b7
Sebastian Willenbrink <sebastian.willenbrink@tum.de>
2023-03-07 10:20:40 +0100

    Readd table for distances

--- agglomerative.ml
+++ agglomerative.ml
@@ -16,26 +16,41 @@ module Make =
     (S : Element_set with type elt = E.t)
     ->
     struct
-      type cluster = { index: int; merged_at: float; set: S.t; children: (cluster * cluster) option; }
+      type cluster = { uid: int; merged_at: float; set: S.t; children: (cluster * cluster) option; }
 
-      let precompute_dist len =
-        let len_total = len * 2 - 1 in
-        let table = Array.init len_total (fun i ->
-            Array.init len_total (fun j ->
-                if i = j
-                then Some 0.0
-                else None
-              ))
-        in
-        fun clusters c1 c2 ->
-          let i,j = min c1.index c2.index, max c1.index c2.index in
-          match table.(i).(j) with
-          | Some dist -> dist
-          | None ->
-            let dist = S.dist (List.nth clusters i).set (List.nth clusters j).set in
-            table.(i).(j) <- Some dist;
-            table.(j).(i) <- Some dist;
-            dist
+      let uid =
+        let x = ref (-1) in
+        fun () ->
+          incr x ;
+          !x
+
+      let mkcluster set = { set; merged_at = 0.; children = None; uid = uid () }
+
+      (* Hash distance computation between clusters. *)
+      module Table = Hashtbl.Make (struct
+          type t = cluster * cluster
+
+          let equal (c1, c2) (c1', c2') =
+            (c1.uid = c1'.uid && c2.uid = c2'.uid)
+            || (c1.uid = c2'.uid && c2.uid = c1'.uid)
+
+          let hash (c1, c2) =
+            if c1.uid < c2.uid then Hashtbl.hash (c1.uid, c2.uid)
+            else Hashtbl.hash (c2.uid, c1.uid)
+        end)
+
+      let dist sz =
+        let table = Table.create sz in
+        fun c1 c2 ->
+          let c1, c2 = if c1.uid < c2.uid then c1, c2 else c2, c1 in
+          if c1.uid = c2.uid then 0.0
+          else
+            match Table.find_opt table (c1, c2) with
+            | Some dist -> dist
+            | None ->
+              let dist = S.dist c1.set c2.set in
+              Table.add table (c1, c2) dist ;
+              dist
 
       let minimum_pairwise_distance (dist : cluster -> cluster -> float) clusters =
         match clusters with
@@ -44,32 +59,33 @@ module Make =
           let seq = List.to_seq clusters |> (fun x -> Seq.product x x) in
           Seq.fold_left (fun ((dist_min, _, _) as acc) (a,b) ->
               let dist_new = dist a b in
-              if a.index = b.index || dist_new > dist_min
+              if a.uid = b.uid || dist_new > dist_min
               then acc
               else (dist_new, a, b)
             ) (dist c1 c2, c1, c2) seq
 
       let cluster_with_initial element_sets =
-        let dist = precompute_dist (List.length element_sets) in
+        (* The number of sets in total is |elements| * 2 - 1 because each new cluster combines two elements. *)
+        (* We end with exactly one cluster remaining. *)
+        let dist = dist (List.length element_sets * 2 - 1) in
         let len = List.length element_sets in
-        let clusters =
-          List.mapi (fun index set -> {index; merged_at = 0.; set; children = None; }) element_sets;
-        in
+        let clusters = List.map mkcluster element_sets in
 
-        let rec iterate index clusters active_clusters =
+        let rec iterate index active_clusters =
           match active_clusters with
           | [] -> failwith "No cluster left."
           | [c] -> c
           | _ ->
-            let merged_at, left, right = minimum_pairwise_distance (dist clusters) active_clusters in
+            let merged_at, left, right = minimum_pairwise_distance dist active_clusters in
             let set = S.join left.set right.set in
-            let cluster_new = {index; merged_at; set; children = Some (left, right); } in
-            let clusters = clusters @ [cluster_new] in
-            let active_clusters = cluster_new :: List.filter (fun x -> x.index != left.index && x.index != right.index) active_clusters in
-            Printf.printf "Joined %i %i\n" left.index right.index;
-            iterate (index+1) clusters active_clusters
+            let cluster_new = {uid = uid (); merged_at; set; children = Some (left, right); } in
+            let active_clusters =
+              cluster_new
+              :: List.filter (fun x -> x.uid != left.uid && x.uid != right.uid) active_clusters
+            in
+            iterate (index+1) active_clusters
         in
-        iterate len clusters clusters
+        iterate len clusters
 
       let cluster elements =
         List.map (fun x -> S.singleton x) elements
